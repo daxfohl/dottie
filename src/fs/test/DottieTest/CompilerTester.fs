e3d67009220f8aa2fe91e556ce81e8319af9d3a7
Dax Fohl <dax@fohlsw.com>
2019-02-05 12:36:09 -0800

    Rename test

---
+++ src/fs/test/DottieTest/CompilerTester.fs
@@ -0,0 +1,25 @@
+﻿module ``Compiler tester``
+open Xunit
+open Compiler
+
+[<Fact>]
+let ``Test Compiler``() =
+  let fileStringFFI = """
+  foreign module StringFFI {
+    concat: fun { s1: rawstring s2: rawstring } -> rawstring
+  } """
+  let parsed = parse fileStringFFI
+  let expected =
+    Choice1Of2
+      (ForeignModule
+          {name = "StringFFI";
+          definitions =
+            [{name = "concat";
+              spec =
+              Function
+                { input = Object [{name = "s2"
+                                   spec = Raw RawString};
+                                  {name = "s1"
+                                   spec = Raw RawString}]
+                  output = Raw RawString}}]}, [])
+  Assert.Equal(expected, parsed)
