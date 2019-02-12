e7524c80b3aa4afa6c90ee5cfbab8a3d6b891ebf
Dax Fohl <dax@fohlsw.com>
2019-02-11 18:11:16 -0800

    More tests

--- src/fs/test/DottieTest/ExpressionParserTester.fs
+++ src/fs/test/DottieTest/ExpressionParserTester.fs
@@ -51,3 +51,17 @@ let ``Test function deep``() =
   let parsed = parseExpression strings
   let expected = Choice1Of2 (FunctionApplication(Subfield(Subfield(Variable "a", "b"), "f"), Subfield(Subfield(Variable "x", "y"), "z")), [])
   Assert.Equal(expected, parsed)
+
+[<Fact>]
+let ``Test function function``() =
+  let strings = tokenize """f g x"""
+  let parsed = parseExpression strings
+  let expected = Choice1Of2 (FunctionApplication(Variable "f", FunctionApplication(Variable "g", Variable "x")), [])
+  Assert.Equal(expected, parsed)
+
+[<Fact>]
+let ``Test function function deep``() =
+  let strings = tokenize """a.b.f c.d.g x.y.z"""
+  let parsed = parseExpression strings
+  let expected = Choice1Of2 (FunctionApplication(Subfield(Subfield(Variable "a", "b"), "f"), FunctionApplication(Subfield(Subfield(Variable "c", "d"), "g"), Subfield(Subfield(Variable "x", "y"), "z"))), [])
+  Assert.Equal(expected, parsed)
