7671a15b11fdd4d38707f2ba24546518d8a9678e
Dax Fohl <dax@fohlsw.com>
2019-02-11 17:27:56 -0800

    Objects work in ExpressionParser

--- src/fs/src/Dottie/ExpressionParser.fs
+++ src/fs/src/Dottie/ExpressionParser.fs
@@ -12,7 +12,7 @@ type Definition =
     expression: Expression }
 and Expression =
   | Import of string
-  | Subfield of string * string
+  | Subfield of Expression * string
   | Object of list<Definition>
   | ObjectWith of string * list<Definition>
   | FunctionDefintion of string * list<Statement> * string
@@ -23,65 +23,21 @@ and Statement =
   | Assignment of Definition
   | Return of Expression
 
+let validIdentifier (s: string) = s <> null && s.Length <> 0 && Char.IsLetter(s.[0]) && s |> Seq.forall Char.IsLetterOrDigit
 
-let rec parseDefinition (tokens: string list) : Choice<Definition * string list, string> =
-  match tokens with
-  | "let"::name::"="::t ->
-    match parseExpression t with
-    | Choice1Of2 (expression, t) ->
-      Choice1Of2({name=name; expression=expression}, t)
-    | Choice2Of2 x -> Choice2Of2 x
-  | h::m::_ -> Choice2Of2 <| sprintf "parseDefinition got %s %s" h m
-  | h::_ -> Choice2Of2 <| sprintf "parseDefinition got %s end" h
-  | [] -> Choice2Of2 "parseDefinition got empty list"
-and parseExpression (tokens: string list) : Choice<Expression * string list, string> =
-  match tokens with
-  | "import"::name::";"::t -> Choice1Of2(Import name, t)
-  | object::"."::field::";"::t -> Choice1Of2(Subfield(object, field), t)
-  | "{"::name::"with"::t ->
-    match parseObject t with
-    | Choice1Of2 (properties, t) -> Choice1Of2 (ObjectWith(name, properties), t)
-    | Choice2Of2 x -> Choice2Of2 x
-  | "{"::t ->
-    match parseObject t with
-    | Choice1Of2 (properties, t) -> Choice1Of2 (Object properties, t)
-    | Choice2Of2 x -> Choice2Of2 x
-  | "fun"::input::"->"::"{"::t ->
-    match parseFunction t with
-    | Choice2Of2 x -> Choice2Of2 x
-    | Choice1Of2 (statements, retval, t) -> Choice1Of2(FunctionDefintion(input, List.rev statements, retval), t)
-  | f::name::";"::t -> Choice1Of2(FunctionApplication(f, name), t)
-  | "\""::s::"\""::";"::t -> Choice1Of2(Constant(RawString s), t)
-  | s::";"::t when let b, _ = Double.TryParse s in b -> Choice1Of2(Constant(RawNumber(Double.Parse s)), t)
-  | s::";"::t -> Choice1Of2(Variable s, t)
-  | h::t -> Choice2Of2 <| sprintf "parseSpec got %s" h
-  | [] -> Choice2Of2 "parseSpec got empty list"
-and parseFunction (tokens: string list) : Choice<list<Statement> * string * string list, string> =
-  let rec addStatements = fun tokens statements ->
-    match tokens with
-    | retval::";"::"}"::";"::t -> Choice1Of2(statements, retval, t)
-    | _ ->
-      match parseDefinition tokens with
-      | Choice2Of2 x -> Choice2Of2 x
-      | Choice1Of2 (definition, tokens) -> addStatements tokens ((Assignment definition)::statements)
-  addStatements tokens []
-and parseObject (tokens: string list) : Choice<list<Definition> * string list, string> =
-  let rec addFields = fun tokens fields ->
-    match parseField tokens with
-    | Choice2Of2 x -> Choice2Of2 x
-    | Choice1Of2 (definition, tokens) ->
-      let declarations = definition::fields
+let parseExpression (tokens: string list) : Choice<Expression * string list, string> =
+  let rec parseExpression' (tokens: string list) (expr: option<Expression>) : Choice<Expression * string list, string> =
+    let parseExpression tokens expr = parseExpression' tokens (Some expr)
+    match expr with
+    | None ->
       match tokens with
-      | "}"::";"::t -> Choice1Of2(declarations, t)
-      | _ -> addFields tokens declarations
-  addFields tokens []
-and parseField (tokens: string list) : Choice<Definition * string list, string> =
-  match tokens with
-  | name::":"::t ->
-    match parseExpression t with
-    | Choice1Of2 (expression, t) ->
-      Choice1Of2({name=name; expression=expression}, t)
-    | Choice2Of2 x -> Choice2Of2 x
-  | h::m::_ -> Choice2Of2 <| sprintf "parseField got %s %s" h m
-  | h::_ -> Choice2Of2 <| sprintf "parseField got %s end" h
-  | [] -> Choice2Of2 "parseField got empty list"
+      | s::t when validIdentifier s -> parseExpression t (Variable s)
+      | h::_ -> Choice2Of2 <| sprintf "parseExpression got %s" h
+      | [] -> Choice2Of2 "parseExpression got empty list"
+    | Some expr ->
+      match tokens with
+      | ";"::t -> Choice1Of2(expr, t)
+      | "."::s::t when validIdentifier s -> parseExpression t (Subfield (expr, s))
+      | h::_ -> Choice2Of2 <| sprintf "parseExpression got %s" h
+      | [] -> Choice2Of2 "parseExpression got empty list"
+  parseExpression' tokens None
--- src/fs/test/DottieTest/ExpressionParserTester.fs
+++ src/fs/test/DottieTest/ExpressionParserTester.fs
@@ -4,9 +4,22 @@ open ExpressionParser
 open Tokenizer
 
 [<Fact>]
-let ``Test Import``() =
-  let strings = tokenize """import StringFFI;"""
+let ``Test var``() =
+  let strings = tokenize """x"""
   let parsed = parseExpression strings
-  let expected =
-    Choice1Of2 (Import "StringFFI", [])
+  let expected = Choice1Of2 (Variable "x", [])
+  Assert.Equal(expected, parsed)
+  
+[<Fact>]
+let ``Test subfield``() =
+  let strings = tokenize """x.y"""
+  let parsed = parseExpression strings
+  let expected = Choice1Of2 (Subfield(Variable "x", "y"), [])
+  Assert.Equal(expected, parsed)
+
+[<Fact>]
+let ``Test sub-subfield``() =
+  let strings = tokenize """x.y.z"""
+  let parsed = parseExpression strings
+  let expected = Choice1Of2 (Subfield(Subfield(Variable "x", "y"), "z"), [])
   Assert.Equal(expected, parsed)
