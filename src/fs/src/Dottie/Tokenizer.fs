9681c1f1bfce3b4706db9d1b36c84f2c355e31f1
Dax Fohl <dax@fohlsw.com>
2019-02-15 22:39:09 -0800

    Clean up tokenizer, don't force space between symbols

--- src/fs/src/Dottie/Tokenizer.fs
+++ src/fs/src/Dottie/Tokenizer.fs
@@ -4,44 +4,55 @@ open System
 open System.Collections.Generic
 open System.Text.RegularExpressions
 
-type CharType = AlphaNumeric | Symbol
-
 let addSemicolons(file: string) =
   let lines = Regex.Split(file, "\r\n|\r|\n")
   let notEndings = ['['; '{'; '=']
   let addSemi (line: string) = line.Length > 0 && not(List.contains line.[line.Length - 1] notEndings)
   String.Join("\n", Array.map (fun line -> if addSemi line then line + " ;" else line) lines)
 
+let twoCharSymbols = [["-";">"]]
+
+let joinSymbols (tokens: string list) =
+  let rec joinSymbols (newTokens: string list) =
+    function
+    | [] -> List.rev newTokens
+    | "-"::">"::t -> joinSymbols ("->"::newTokens) t
+    | x::t -> joinSymbols (x::newTokens) t
+  joinSymbols [] tokens
+
+let commaToSemi = List.map (fun t -> if t = "," then ";" else t)
+
+let insertSemicolon (tokens: string list) =
+  let rec insertSemicolon (newTokens: string list) =
+    function
+    | [] -> List.rev newTokens
+    | "}"::t -> insertSemicolon ("}"::";"::newTokens) t
+    | x::t -> insertSemicolon (x::newTokens) t
+  insertSemicolon [] tokens
+
+let removeDuplicateSemicolons (tokens: string list) =
+  let rec removeDuplicateSemicolons (newTokens: string list) =
+    function
+    | [] -> List.rev newTokens
+    | ";"::";"::t -> removeDuplicateSemicolons (";"::newTokens) t
+    | "{"::";"::"}"::t -> removeDuplicateSemicolons ("}"::"{"::newTokens) t
+    | x::t -> removeDuplicateSemicolons (x::newTokens) t
+  removeDuplicateSemicolons [] tokens
 
 let tokenize (file: string) =
   let file = addSemicolons file
   let currentToken = List<char>()
   let tokens = List<string>()
-  let charType c = if Char.IsLetterOrDigit(c) then AlphaNumeric else Symbol
-  let mutable state = None
   let complete() =
-    let token = String(currentToken.ToArray())
-    let token = if token = "," then ";" else token
-    if tokens.Count <> 0 && tokens.[tokens.Count - 1] <> ";" && tokens.[tokens.Count - 1] <> "{" && token = "}" then tokens.Add(";")
-    tokens.Add(token)
-    currentToken.Clear()
-    state <- None
-  let start c =
-    currentToken.Add(c)
-    state <- Some(charType(c))
+    if currentToken.Count <> 0 then
+      let token = String(currentToken.ToArray())
+      tokens.Add(token)
+      currentToken.Clear()
   for c in file do
-    if Char.IsWhiteSpace(c) then
-      if state <> None then
-        complete()
+    if Char.IsWhiteSpace(c) then complete()
+    else if Char.IsLetterOrDigit(c) then currentToken.Add(c)
     else
-      match state with
-      | None -> start(c)
-      | Some tokenType ->
-        let charType = charType(c)
-        if charType = tokenType then
-          currentToken.Add(c)
-        else
-          complete()
-          start(c)
-  if currentToken.Count <> 0 then complete()
-  tokens |> List.ofSeq
+      complete()
+      tokens.Add(String(c, 1))
+  complete()
+  tokens |> List.ofSeq |> joinSymbols |> commaToSemi |> insertSemicolon |> removeDuplicateSemicolons
