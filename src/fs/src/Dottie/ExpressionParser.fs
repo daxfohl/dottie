e504ebe8703ec23c0a8da3f8a13b22e2bd19217c
Dax Fohl <dax@fohlsw.com>
2019-02-21 22:30:15 -0800

    Use choose for ExpressionParser too

--- src/fs/src/Dottie/ExpressionParser.fs
+++ src/fs/src/Dottie/ExpressionParser.fs
@@ -2,6 +2,7 @@
 
 open System
 open Expressions
+open FSharpx.Choice
 
 let keywords =
   [ "import"
@@ -19,68 +20,62 @@ let canStartExpression (s: string) = s <> ";"
 
 let rec parseExpression (tokens: string list) : Choice<Expr * string list, string> =
   let rec parseLetBlock (tokens: string list) : Choice<Expr * string list, string> =
-    match tokens with
-    | "let"::name::"="::t ->
-      match parseExpression t with
-      | Choice1Of2 (expr, t) ->
-        match parseLetBlock t with
-        | Choice1Of2 (rest, t) -> Choice1Of2(LetExpr(name, expr, rest), t)
-        | Choice2Of2 s -> Choice2Of2 s
-      | Choice2Of2 s -> Choice2Of2 s
-    | _ ->
-      match parseExpression tokens with
-      | Choice1Of2(expr, t) ->
+    choose {
+      match tokens with
+      | "let"::name::"="::t ->
+        let! expr, t = parseExpression t
+        let! rest, t = parseLetBlock t
+        return LetExpr(name, expr, rest), t
+      | _ ->
+        let! expr, t = parseExpression tokens
         match t with
-        | "}"::t -> Choice1Of2(expr, t)
-        | s::_ -> Choice2Of2(sprintf "parseLetBlock expected '}' but got '%s'" s)
-        | [] -> Choice2Of2 "parseLetBlock expected '}' but got EOF"
-      | Choice2Of2 s -> Choice2Of2 s
+        | "}"::t -> return expr, t
+        | s::_ -> return! Choice2Of2(sprintf "parseLetBlock expected '}' but got '%s'" s)
+        | [] -> return! Choice2Of2 "parseLetBlock expected '}' but got EOF" }
   let rec parseObjectFields (tokens: string list) (object: Map<string, Expr>) : Choice<Map<string, Expr> * string list, string> =
-    match tokens with
-    | "}"::t -> Choice1Of2 (object, t)
-    | s::":"::t ->
-      match parseExpression t with
-      | Choice1Of2(expr, t') -> parseObjectFields t' (Map.add s expr object)
-      | Choice2Of2 s -> Choice2Of2 s
-    | s::m::_ -> Choice2Of2 <| sprintf "parseObjectFields expected name:, but got %s %s" s m
-    | [s] -> Choice2Of2 <| sprintf "parseObjectFields expected name:, but got %s EOF" s
-    | [] -> Choice2Of2 <| sprintf "parseObjectFields expected name:, but got EOF"
+    choose {
+      match tokens with
+      | "}"::t -> return object, t
+      | s::":"::t ->
+        let! expr, t' = parseExpression t
+        return! parseObjectFields t' (Map.add s expr object)
+      | s::m::_ -> return! Choice2Of2 <| sprintf "parseObjectFields expected name:, but got %s %s" s m
+      | [s] -> return! Choice2Of2 <| sprintf "parseObjectFields expected name:, but got %s EOF" s
+      | [] -> return! Choice2Of2 <| sprintf "parseObjectFields expected name:, but got EOF" }
   let rec parseContinuation (tokens: string list) (expr: Expr) : Choice<Expr * string list, string> =
-    match tokens with
-    | [] -> Choice1Of2(expr, tokens)
-    | ";"::t -> Choice1Of2(expr, t)
-    | "."::t ->
-      match t with
-      | s::t when validIdentifier s -> parseContinuation t (DotExpr (expr, s))
-      | s::_ -> Choice2Of2 <| sprintf "expected identifier after dot but got %s" s
-      | [] -> Choice2Of2 "got nothing after dot"
-    | s::_ when canStartExpression s ->
-      match parseExpression tokens with
-      | Choice1Of2(e, t) -> Choice1Of2 (EvalExpr(expr, e), t)
-      | Choice2Of2 s -> Choice2Of2 s
-    | h::_ -> Choice2Of2 <| sprintf "parseContinuation got %s" h
+    choose {
+      match tokens with
+      | [] -> return expr, tokens
+      | ";"::t -> return expr, t
+      | "."::t ->
+        match t with
+        | s::t when validIdentifier s -> return! parseContinuation t (DotExpr (expr, s))
+        | s::_ -> return! Choice2Of2 <| sprintf "expected identifier after dot but got %s" s
+        | [] -> return! Choice2Of2 "got nothing after dot"
+      | s::_ when canStartExpression s ->
+        let! e, t = parseExpression tokens
+        return EvalExpr(expr, e), t
+      | h::_ -> return! Choice2Of2 <| sprintf "parseContinuation got %s" h }
   match tokens with
   | s::t when validIdentifier s -> parseContinuation t (ValExpr s)
   | "import"::name::t -> parseContinuation t (ImportExpr name)
   | "\""::s::"\""::t -> parseContinuation t (LitExpr(StrExpr s))
   | s::t when let b, _ = Int32.TryParse s in b -> parseContinuation t (LitExpr(IntExpr(Int32.Parse s)))
   | "{"::t ->
-    match t with
-    | "let"::_::"="::_ ->
-      match parseLetBlock t with
-      | Choice1Of2(expr, t) -> parseContinuation t expr
-      | Choice2Of2 s -> Choice2Of2 s
-    | name::"with"::t ->
-      match parseObjectFields t Map.empty with
-      | Choice1Of2(expr, t) -> parseContinuation t (WithExpr(name, expr))
-      | Choice2Of2 s -> Choice2Of2 s
-    | _ ->
-      match parseObjectFields t Map.empty with
-      | Choice1Of2(expr, t) -> parseContinuation t (ObjExpr expr)
-      | Choice2Of2 s -> Choice2Of2 s
+    choose {
+      match t with
+      | "let"::_::"="::_ ->
+        let! expr, t = parseLetBlock t
+        return! parseContinuation t expr
+      | name::"with"::t ->
+        let! expr, t = parseObjectFields t Map.empty
+        return! parseContinuation t (WithExpr(name, expr))
+      | _ ->
+        let! expr, t = parseObjectFields t Map.empty
+        return! parseContinuation t (ObjExpr expr) }
   | "fn"::name::"->"::t ->
-    match parseExpression t with
-    | Choice1Of2 (expr, t) -> Choice1Of2(FnExpr(name, expr), t)
-    | Choice2Of2 s -> Choice2Of2 s
+    choose {
+      let! expr, t = parseExpression t
+      return FnExpr(name, expr), t }
   | h::_ -> Choice2Of2 <| sprintf "parseExpression got %s" h
   | [] -> Choice2Of2 "parseExpression got empty list"
