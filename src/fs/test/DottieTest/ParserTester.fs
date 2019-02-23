f742a99f62828fcf43965b757532a2db551e5dda
Dax Fohl <dax@fohlsw.com>
2019-02-22 23:02:31 -0800

    test passes

--- src/fs/test/DottieTest/ParserTester.fs
+++ src/fs/test/DottieTest/ParserTester.fs
@@ -1,23 +1,21 @@
 ﻿module ``Parser tester``
 open Xunit
+open TypeInferencer2
 open SpecParser
 open Tokenizer
 
 [<Fact>]
 let ``Test Parser``() =
   let fileStringFFI = """
-  { concat: fn { s1: rawstring, s2: rawstring } -> rawstring }"""
+  { concat: fn { s1: literal string, s2: literal string } -> literal string }"""
   let parsed = parseRawSpec (tokenize fileStringFFI)
   let expected =
     Choice1Of2
-      (Object
-            [{name = "concat";
-              spec =
-              Function
-                { input = Object [{name = "s2"
-                                   spec = Raw RawString};
-                                  {name = "s1"
-                                   spec = Raw RawString}]
-                  output = Raw RawString}}], [])
+      (ObjSpec ^% Map.ofList
+            [("concat",
+               FnSpec(
+                 ObjSpec ^% Map.ofList [("s2", LitSpec StrSpec);
+                                        ("s1", LitSpec StrSpec)],
+                 LitSpec StrSpec))], [";"])
 
   Assert.Equal(expected, parsed)
