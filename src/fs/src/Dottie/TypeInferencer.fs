cc015f9df9c25239611e6161d8fc03f8dfe40a9c
Dax Fohl <dafohl@microsoft.com>
2019-03-11 15:26:52 -0700

    First attempt at fix

--- src/fs/src/Dottie/TypeInferencer.fs
+++ src/fs/src/Dottie/TypeInferencer.fs
@@ -81,16 +81,16 @@ let rec unify (spec1: Spec) (spec2: Spec): Choice<(Expr * Spec) list, string> =
             return (expr2, FreeFnSpec(expr2, merged, output2))::(expr1, FreeFnSpec(expr1, merged, output1))::changes
           | _ -> return! err }
 
-let rec replace (expr: Expr) (replacement: Spec) (domain: Spec) =
-  match domain with
+let rec replace (expr: Expr) (replacement: Spec) (existing: Spec) =
+  match existing with
   | FreeSpec expr1 when expr = expr1 -> replacement
   | FreeObjSpec(expr1, _) when expr = expr1 -> replacement
   | FreeFnSpec(expr1, _, _) when expr = expr1 -> replacement
   | ObjSpec fields -> ObjSpec(Map.map (fun _ -> replace expr replacement) fields)
   | FreeObjSpec(expr1, fields) -> FreeObjSpec(expr1, Map.map (fun _ -> replace expr replacement) fields)
-  | FreeFnSpec(expr1, fields, result1) -> failwith "No idea what to do here"//FreeFnSpec(expr1, Map.map (fun _ -> replace expr replacement) fields, result1)
+  | FreeFnSpec(expr1, fields, result1) -> FreeFnSpec(expr1, Map.map (fun _ -> replace expr replacement) fields, result1)
   | FnSpec(input, output) -> FnSpec(replace expr replacement input, replace expr replacement output)
-  | _ -> domain
+  | _ -> existing
 
 let replaceInMap (expr: Expr, replacement: Spec) = Map.map (fun _ -> replace expr replacement)
 
