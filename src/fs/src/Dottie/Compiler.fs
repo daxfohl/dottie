ed4c4e94ba118ae14b82be8b3991a35e50f0be17
Dax Fohl <dax@fohlsw.com>
2019-04-01 08:09:34 -0700

    tests

--- src/fs/src/Dottie/Compiler.fs
+++ src/fs/src/Dottie/Compiler.fs
@@ -2,7 +2,17 @@
 
 open Expressions
 
-let rec compile (expr: E) : string =
+let rec compileRest rest =
+  match rest with
+  | ELet _ -> compile rest
+  | _ -> sprintf "return %s;\n" (compile rest)
+
+and wrap expr =
+  match expr with
+  | ELet _ -> sprintf "(() => { %s })()" (compileRest expr)
+  | _ -> compile expr
+
+and compile (expr: E) : string =
   match expr with
   | ELit x ->
     match x with 
@@ -10,20 +20,20 @@ let rec compile (expr: E) : string =
     | EInt i -> i.ToString()
   | EVal s -> s
   | ELet(s, expr, rest) ->
-    sprintf "let %s = %s;\n%s" s (compile expr) (compile rest)
+    sprintf "let %s = %s;\n%s" s (wrap expr) (compileRest rest)
   | EFn(input, expr, async) ->
     if async
-      then sprintf "async %s => { %s }" input (compile expr)
-      else sprintf "%s => { %s }" input (compile expr)
+      then sprintf "async %s => { %s }" input (compileRest expr)
+      else sprintf "%s => { %s }" input (compileRest expr)
   | EObj fields ->
-    sprintf "{ %s }" (String.concat ",\n" (fields |> Map.toList |> List.map (fun (k, v) -> sprintf "%s: %s" k (compile v))))
+    sprintf "{ %s }" (String.concat ",\n" (fields |> Map.toList |> List.map (fun (k, v) -> sprintf "%s: %s" k (wrap v))))
   | EWith(orig, fields) ->
-    let orig = compile orig
-    let fields = compile (EObj fields)
-    sprintf "(() => { let target = {}; Object.assign(target, %s); Object.assign(target, %s); return target; })();" orig fields
+    let orig = wrap orig
+    let fields = wrap (EObj fields)
+    sprintf "(() => { let target = {}; Object.assign(target, %s); Object.assign(target, %s); return target; })()" orig fields
   | EDot(objExpr, field) ->
-    sprintf "%s.%s" (compile objExpr) field
+    sprintf "%s.%s" (wrap objExpr) field
   | EEval(fn, arg) ->
-    sprintf "%s(%s)" (compile fn) (compile arg)
+    sprintf "%s(%s)" (wrap fn) (wrap arg)
   | EDo expr ->
     sprintf "await %s" (compile expr)
---
+++ src/fs/test/DottieTest/CompilerTester.fs
@@ -0,0 +1,62 @@
+﻿module ``Compiler tester``
+
+open Xunit
+open Tokenizer
+open Expressions
+open ExpressionParser
+open Compiler
+open System.Text.RegularExpressions
+
+let get choice =
+  match choice with
+  | Choice1Of2 (x, _) -> x
+  | Choice2Of2 x -> failwith x
+  
+let assertSpec(sExpr, s) =
+  let strings = tokenize sExpr
+  let expr = get ^% parseExpression strings
+  let compiled = compile expr
+  let s1 = Regex.Replace(compiled, @"\s+", "")
+  let s2 = Regex.Replace(s, @"\s+", "")
+  Assert.Equal(s2, s1)
+
+[<Fact>]
+let ``Test number``() =
+  assertSpec("2", "2")
+
+[<Fact>]
+let ``Test string``() =
+  assertSpec("\"test\"", "\"test\"")
+  
+[<Fact>]
+let ``Test let``() =
+  assertSpec("{ let x = 3; x }", "let x = 3; return x;")
+
+[<Fact>]
+let ``Test let two``() =
+  assertSpec("{ let x = 3; let y = x; y }", "let x = 3; let y = x; return y;")
+
+[<Fact>]
+let ``Test let nested``() =
+  assertSpec("{ let z = { let x = 3; let y = x; y }; z }", "let z = (()=>{ let x = 3; let y = x; return y; })(); return z;")
+
+[<Fact>]
+let ``Test obj``() =
+  assertSpec("{ x: 4 }", "{ x: 4 }")
+
+[<Fact>]
+let ``Test obj empty``() =
+  assertSpec("{}","{}")
+
+[<Fact>]
+let ``Test obj two``() =
+  assertSpec(
+    "{ x: 4; y: \"test\" }",
+    "{ x: 4, y: \"test\" }")
+
+[<Fact>]
+let ``Test obj with``() =
+  assertSpec(
+    "{ let o = { x: 4; y: \"test\" }; { o with x: 5 } }",
+    "let o = { x: 4, y: \"test\" };
+      return (() => { let target = {}; Object.assign(target, o); Object.assign(target, { x: 5 }); return target; })();")
