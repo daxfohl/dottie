64b05be3012d054443c938af02e6f82e115180cf
Dax Fohl <dax@fohlsw.com>
2019-10-12 18:43:15 -0700

    CompileFile

--- src/fs/src/Dottie/Compiler.fs
+++ src/fs/src/Dottie/Compiler.fs
@@ -7,11 +7,13 @@ open TypeInferencer
 open SpecParser
 open FSharpx.Choice
 
-type M =
-| Module of string * E * S
-| ForeignModule of string * S
+type MType =
+| Module of E * S
+| ForeignModule of S
 
-type ModuleMap = Map<string, M>
+type M = string * MType
+
+type ModuleMap = Map<string, MType>
 
 let compileModule (tokens: string list) (moduleMap: ModuleMap): Choice<M * string list, string> =
   choose {
@@ -19,10 +21,22 @@ let compileModule (tokens: string list) (moduleMap: ModuleMap): Choice<M * strin
     | "module"::name::tokens ->
       let! e, tokens = parseExpression tokens
       let! s, _ = getType e Map.empty Normal
-      return Module(name, e, s), tokens
+      return (name, Module(e, s)), tokens
     | "foreign"::"module"::name::tokens ->
       let! s, tokens = parseRawSpec tokens
-      return ForeignModule(name, s), tokens
+      return (name, ForeignModule(s)), tokens
     | [] -> return! Choice2Of2 "EOF"
     | h::t -> return! Choice2Of2 (sprintf "Expected module, got %s" h)
   }
+
+let rec compileModules (tokens: string list) (moduleMap: ModuleMap): Choice<ModuleMap, string> =
+  choose {
+    match tokens with
+    | [] -> return moduleMap
+    | _ ->
+      let! (name, mType), tokens = compileModule tokens moduleMap
+      let moduleMap = moduleMap.Add(name, mType)
+      return! compileModules tokens moduleMap
+  }
+
+let compileFile tokens = compileModules tokens Map.empty
