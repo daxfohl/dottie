ea0229f5e20c41f22b1e4a5f2aca7291fef88ca3
Dax Fohl <dafohl@microsoft.com>
2019-10-23 11:06:48 -0700

    Fix number parsing, comments

--- src/fs/src/Dottie/Tokenizer.fs
+++ src/fs/src/Dottie/Tokenizer.fs
@@ -30,6 +30,8 @@ type Token =
 | KProc
 | KFn
 | KLiteral
+| KModule
+| KForeign
 | KName of string
 | KEquals
 | KOpenCurly
@@ -37,8 +39,6 @@ type Token =
 | KSemicolon
 | KDot
 | KColon
-| KModule
-| KForeign
 | KArrow
 | KNumber of float
 | KString of string
@@ -47,7 +47,7 @@ type Token =
 
 type PageToken = {
   row: int
-  startCol: int
+  col: int
   len: int
   value: Token
 }
@@ -57,7 +57,7 @@ let insertSemicolon (tokens: PageToken list): PageToken list =
     function
     | [] -> List.rev newTokens
     | x::t ->
-      let fakeSemi = { row = x.row; len = 0; startCol = x.startCol + x.len; value = KSemicolon }
+      let fakeSemi = { row = x.row; len = 0; col = x.col + x.len; value = KSemicolon }
       match x.value with
       | KClosedCurly -> insertSemicolon (x::fakeSemi::newTokens) t // For consistency finding end of expr in { a: 1; b: 2 }, it is  { a: 1; b: 2; }
       | KWith -> insertSemicolon (x::fakeSemi::newTokens) t // To find end of expr in { <expr> with ... }, it's { <expr>; with ... }
@@ -77,10 +77,10 @@ let removeDuplicateSemicolons (tokens: PageToken list) =
 // negative number
 // decimals
 // comments
-// operators (binary, unary -) // well, are we doing this or having ".plus"?
+// operators // well, are we doing this or having ".plus"?
 type State =
 | Empty // empty, in name, in symbol
-| InNumber
+| InNumber of bool
 | InString of bool
 | InComment
 | InWhitespace
@@ -113,10 +113,7 @@ let createToken(currentToken: char seq, state: State, lineNumber: int, charNumbe
       | ":" -> KColon
       | "->" -> KArrow
       | _ -> KError tokenStr
-    | InNumber ->
-      match Double.TryParse(tokenStr) with
-      | true, f -> KNumber f
-      | _ -> KError tokenStr
+    | InNumber _ -> KNumber ^% Double.Parse(tokenStr)
     | InString true -> KError tokenStr
     | InString false ->
       let rest = tokenStr.Substring(1)
@@ -126,7 +123,7 @@ let createToken(currentToken: char seq, state: State, lineNumber: int, charNumbe
     | InWhitespace
     | Empty -> failwith "Cannot create token here"
   { row = lineNumber
-    startCol = charNumber - tokenStr.Length
+    col = charNumber - tokenStr.Length
     len = tokenStr.Length
     value = token }
 
@@ -135,14 +132,24 @@ let getNextState = function
 | Some c, _ when Char.IsWhiteSpace(c) -> InWhitespace
 | Some c, _ when c = '"' -> InString false
 | Some c, Some c1 when c = '/' && c1 = '/' -> InComment
-| Some c, _ when Char.IsDigit(c) -> InNumber
-| Some c, Some c1 when c = '-' && Char.IsDigit c1 -> InNumber
+| Some c, _ when Char.IsDigit(c) -> InNumber false
+| Some c, Some c1 when c = '-' && Char.IsDigit c1 -> InNumber false
 | Some c, _ when Char.IsLetter(c) -> InWord
 | Some _, _ -> InSymbol
   
-let complete(currentToken: char IList, tokens: PageToken IList, charNumber: int, lineNumber: int, cNext: char option, cAfter: char option, state: State ref) =
+let addFinalSemicolon (tokens: Stack<PageToken>) =
+  if tokens.Count <> 0 then
+    let lastToken = tokens.Peek()
+    match lastToken.value with
+    | KName _
+    | KClosedCurly
+    | KNumber _
+    | KString _ -> tokens.Push({ row = lastToken.row; len = 0; col = lastToken.col + lastToken.len; value = KSemicolon })
+    | _ -> ()
+
+let complete(currentToken: char IList, tokens: PageToken Stack, charNumber: int, lineNumber: int, cNext: char option, cAfter: char option, state: State ref) =
   if !state <> Empty && !state <> InWhitespace then
-    tokens.Add(createToken(currentToken, !state, lineNumber, charNumber))
+    tokens.Push(createToken(currentToken, !state, lineNumber, charNumber))
   currentToken.Clear()
   match cNext with
   | Some c -> currentToken.Add(c)
@@ -150,12 +157,12 @@ let complete(currentToken: char IList, tokens: PageToken IList, charNumber: int,
   state := getNextState(cNext, cAfter)
 
 let tokenizeLine (line: string, lineNumber: int): PageToken list =
-  let tokens = List<PageToken>()
+  let tokens = Stack<PageToken>()
   let currentToken = List<char>()
   let state = ref Empty
   for charId = 0 to line.Length - 1 do
     let c = line.[charId]
-    let cAfter = if line.Length < charId + 1 then Some line.[charId + 1] else None
+    let cAfter = if line.Length > charId + 1 then Some line.[charId + 1] else None
     let completeAndLoad = fun () -> complete(currentToken, tokens, charId, lineNumber, Some c, cAfter, state)
     let completeOnly = fun () -> complete(currentToken, tokens, charId, lineNumber, None, None, state)
     match !state with
@@ -171,31 +178,33 @@ let tokenizeLine (line: string, lineNumber: int): PageToken list =
       currentToken.Add(c)
       state := InString false
     | InComment -> currentToken.Add(c)
-    | InNumber -> if Char.IsNumber(c) || c = '.' then currentToken.Add(c) else completeAndLoad()
+    | InNumber false ->
+      if Char.IsNumber(c) || c = '.' then
+         currentToken.Add(c)
+         if c = '.' then state := InNumber true
+      else completeAndLoad()
+    | InNumber true ->
+      if Char.IsNumber(c) then currentToken.Add(c)
+      else completeAndLoad()
     | InWord -> if Char.IsLetterOrDigit(c) then currentToken.Add(c) else completeAndLoad()
     | InSymbol ->
       if currentToken.[0] = '-' && c = '>' then currentToken.Add(c)
       else completeAndLoad()
-  complete(currentToken, tokens, line.Length - 1, lineNumber, None, None, state)
-  let lastToken =
-    if tokens.Count = 0 then None
-    else
-      let lastToken = tokens.[tokens.Count - 1]
-      match lastToken.value with
-      | KComment _ ->
-        if tokens.Count = 1 then None
-        else Some tokens.[tokens.Count - 2]
-      | _ -> Some lastToken
-  match lastToken with
+  complete(currentToken, tokens, line.Length, lineNumber, None, None, state)
+  let commentOpt =
+    match tokens.Count with
+    | 0 -> None
+    | _ ->
+      match tokens.Peek().value with
+      | KComment _ -> Some ^% tokens.Pop()
+      | _ -> None
+  
+  addFinalSemicolon(tokens)
+  match commentOpt with
+  | Some comment -> tokens.Push(comment)
   | None -> ()
-  | Some x ->
-    match x.value with
-    | KName _
-    | KClosedCurly
-    | KNumber _
-    | KString _ -> tokens.Add({ row = x.row; len = 0; startCol = x.startCol + x.len; value = KSemicolon })
-    | _ -> ()
-  List.ofSeq tokens
+
+  tokens |> List.ofSeq |> List.rev
   
 
 let tokenize (file: string): PageToken list =
--- src/fs/test/DottieTest/TokenizerTester.fs
+++ src/fs/test/DottieTest/TokenizerTester.fs
@@ -7,16 +7,62 @@ let assertEqual(expected: Token list, input: string) =
   Assert.Equal<Token list>(expected, List.map (fun t -> t.value) output)
 
 [<Fact>]
-let ``Single token``() =
+let ``Name``() =
   assertEqual([KName "a"; KSemicolon], "a ")
   assertEqual([KName "a"; KSemicolon], " a ")
   assertEqual([KName "a"; KSemicolon], "a")
   assertEqual([KName "a"; KSemicolon], " a")
+  
+[<Fact>]
+let ``Symbol``() =
   assertEqual([KDot], ". ")
   assertEqual([KDot], " . ")
   assertEqual([KDot], ".")
   assertEqual([KDot], " .")
 
 [<Fact>]
+let ``Number``() =
+  assertEqual([KNumber 3.; KSemicolon], "3 ")
+  assertEqual([KNumber 3.; KSemicolon], " 3 ")
+  assertEqual([KNumber 3.; KSemicolon], "3")
+  assertEqual([KNumber 3.; KSemicolon], " 3")
+  
+  
+[<Fact>]
+let ``Negative``() =
+  assertEqual([KNumber -3.; KSemicolon], "-3")
+  
+[<Fact>]
+let ``NumberDot``() =
+  assertEqual([KNumber 3.; KDot], "3.0.")
+  
+[<Fact>]
+let ``Equation``() =
+  assertEqual([KName "a"; KNumber -3.13; KSemicolon], "a-3.13")
+  
+[<Fact>]
+let ``Comment``() =
+  assertEqual([KComment "//"], "//")
+  assertEqual([KName "a"; KSemicolon; KComment "//"], "a//")
+  
+[<Fact>]
+let ``Arrow``() =
+  assertEqual([KArrow], "->")
+  assertEqual([KOpenCurly; KArrow; KOpenCurly], "{->{")
+
+[<Fact>]
 let ``Braces``() =
-  assertEqual([KOpenCurly; KClosedCurly; KSemicolon], "{ }")
+  assertEqual([KOpenCurly; KClosedCurly; KSemicolon], "{}")
+  
+  
+[<Fact>]
+let ``Multiline``() =
+  assertEqual([KOpenCurly; KClosedCurly; KSemicolon], """
+{
+
+}""")
+  assertEqual([KNumber 3.; KSemicolon; KNumber 3.; KSemicolon], "3;3;")
+  assertEqual([KNumber 3.; KSemicolon; KNumber 3.; KSemicolon], """
+3
+
+3""")
