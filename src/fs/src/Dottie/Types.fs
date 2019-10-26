837fb982674d7989b91e7bc665ab1136910b5627
Dax Fohl <dax@fohlsw.com>
2019-10-26 10:17:37 -0700

    ok

--- src/fs/src/Dottie/Types.fs
+++ src/fs/src/Dottie/Types.fs
@@ -1,21 +1,22 @@
 ﻿module Types
 
-//open Expressions
+open Expressions
+open FSharpx.Collections
 
-//type SLit = SStr | SInt
+type SLit = SStr | SInt
 
-//type S =
-//| SLit of SLit
-//| SFree of E
-//| SFn of S * S * bool
-//| SObj of Map<string, S>
-//| SFreeObj of E * Map<string, S>
-//| SFreeFn of E * Set<S> * S * bool
+type S =
+| SLit of SLit
+| SFree of E
+| SFn of S * S * bool
+| SObj of Map<string, S>
+| SFreeObj of E * Map<string, S>
+| SFreeFn of E * PersistentHashMap<S, unit> * S * bool
 
-//type Specs = Map<E, S>
+type Specs = PersistentHashMap<E, S>
 
-//type MType =
-//| Module of E
-//| ForeignModule of S
+type MType =
+| Module of E
+| ForeignModule of S
 
-//type M = string * MType
+type M = string * MType
